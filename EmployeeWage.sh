2b1f7f079d0c74f44b9f21aec6e39c9235b24b9f
Pragati215 <dhakanepragati4@gmail.com>
2020-11-02 17:05:51 +0530

    Modified: Employee PartTime Wage is Computed

--- EmployeeWage.sh
+++ EmployeeWage.sh
@@ -11,5 +11,10 @@ Employee=$((RANDOM%2))
 
 Wage_per_hour=20
 Full_day_hour=8
+Part_time_hour=4
+
 Daily_employee_wage=$(($Wage_per_hour*$Full_day_hour))
 echo "Daily Employee Wage - "$Daily_employee_wage
+
+Daily_employee_wage_part=$(($Wage_per_hour*$Part_time_hour))
+echo "Daily Employee Wage Part Time - "$Daily_employee_wage_part
